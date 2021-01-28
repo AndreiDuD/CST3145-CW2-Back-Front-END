411d864fe7fc1653624c43131f12787ca30128ab
AndreiDuD <andrei.duduman123@gmail.com>
2021-01-28 18:41:52 +0000

    added more server functionality

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
 })
 // Get collection name from database
