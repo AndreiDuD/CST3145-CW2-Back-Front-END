448874769b140dd537641bf6c62c88dac97a5ad6
AndreiDuD <andrei.duduman123@gmail.com>
2021-01-29 01:56:17 +0000

    1

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ mongoClient.connect("mongodb+srv://AndreiUser:M0ng0Us3r@cluster0.jdmnf.mongodb.n
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
+app.use(function(req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    next();
 })
 // Get collection name from database
 app.param("collectionName", (req, res, next,
@@ -29,7 +28,7 @@ app.param("collectionName", (req, res, next,
     app.get("/", (req, res, next) => {
         res.send("select a collection with/collection/:collectionName")
     })
-// Get a list of the collection using the collection name used in the url
+// Get ROUTE a list of the collection using the collection name used in the url
     app.get("/collection/:collectionName", (req, res, next)=> {
         req.collection.find({}).toArray((e, results) => {
             if (e) return next(e);
@@ -80,22 +79,16 @@ app.param("collectionName", (req, res, next,
 
     /* TO DO LIST 
 • MongoDB (4%):
-– Have a MongoDB collection for lesson information - minimal fields: topic, price, location, and space (2%);
 – Have a MongoDB collection for order information (2%) - minimal fields: name, phone number, lesson ID, and number of space.
 
 • Node (2%)
-– Include all the required modules in package.json
-– Include the correct start script in package.json
-– Exclude node modules from the back end Git repository
 
 • Middleware (4%)
 – Create a ‘logger’ middleware that output all requests to the server console (2%)
 – Create a static file middleware that returns lesson images or an error message if the image file does not exist. (2%)
 
 • REST API (10%):
-– Connect to MongoDB with native Node.js driver for MongoDB (Mongoose or similar library is not allowed) (2%);
-– A GET route that returns all the lessons (2%);
-– A P OST route that saves a new order to the ‘order’ collection (2%);
+– A POST route that saves a new order to the ‘order’ collection (2%);
 – A P UT route that updates the number of available spaces in the ‘lesson’ collection after an order is submitted (2%).
 – At least one Postman request is created for each routing (2%).
 
